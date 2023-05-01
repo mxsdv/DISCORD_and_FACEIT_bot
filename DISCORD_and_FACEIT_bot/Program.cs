0736fab919cabe512696f24d762aa78513d8bc8c
Maxim <54187545+mxsdv@users.noreply.github.com>
2023-05-01 20:18:52 +0300

    Добавьте файлы проекта.

---
+++ DISCORD_and_FACEIT_bot/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DISCORD_and_FACEIT_bot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
